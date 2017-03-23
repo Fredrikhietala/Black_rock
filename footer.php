f6fbcd960210761968f8be4b8d88f413b523b08c
Fredrikhietala <hietala.fredrik@gmail.com>
2017-03-23 22:01:23 +0100

    Added footer.php

---
+++ footer.php
@@ -0,0 +1,23 @@
+
+<footer class="footer">
+    <div class="container">
+        <p class="text-muted">&copy; Fredrik Hietala 2017
+        <a href="https://github.com/Fredrikhietala/Black_rock"><img src="<?php echo get_template_directory_uri(); ?>/images/github-6-32.png" title="github"/></a></p>
+    </div>
+</footer>
+
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="<?php bloginfo('template_url'); ?>/js/bootstrap.min.js"></script>-->
+
+    </div> <!-- container -->
+
+    <?php wp_footer(); ?>
+
+  </body>
+</html>
